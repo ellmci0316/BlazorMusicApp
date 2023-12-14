9fe4e209eeb522324e53c562e3c99404b640ff4e
ellio <ellio@DESKTOP-38E1283>
2023-12-14 23:30:34 +0100

    Add project files.

---
+++ BlazorMusicApp/Models/MusicDatabaseContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorMusicApp.Models;
+
+public partial class MusicDatabaseContext : DbContext
+{
+    public MusicDatabaseContext()
+    {
+    }
+
+    public MusicDatabaseContext(DbContextOptions<MusicDatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Track> Tracks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MusicDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Track>(entity =>
+        {
+            entity.HasKey(e => e.TrackId).HasName("PK__Tracks__7A74F8C024A1838D");
+
+            entity.Property(e => e.TrackId).HasColumnName("TrackID");
+            entity.Property(e => e.Artist).HasMaxLength(255);
+            entity.Property(e => e.Genre).HasMaxLength(255);
+            entity.Property(e => e.Playlist).HasMaxLength(255);
+            entity.Property(e => e.Title).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ BlazorMusicApp/Models/Track.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorMusicApp.Models;
+
+public partial class Track
+{
+    public int TrackId { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Artist { get; set; }
+
+    public string? Genre { get; set; }
+
+    public string? Playlist { get; set; }
+}
